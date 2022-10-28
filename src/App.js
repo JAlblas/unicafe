f2a71cb3264e617a75ff40b179121e6c28bee30e
Jasper Alblas <just_jasper@hotmail.com>
2022-10-28 20:35:27 +0200

    in progress files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const Display = ({ category, count }) => (
+  <p>{category}: {count}</p>
+)
+
+const Statistics = ({ good, neutral, bad }) => {
+  const sum = good + neutral + bad;
+  const positivePercentage = good / sum * 100;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Statistics</h2>
+      <Display category="good" count={good} />
+      <Display category="neutral" count={neutral} />
+      <Display category="bad" count={bad} />
+      <Display category="all" count={sum} />
+      <Display category="average" count={good - bad} />
+      <Display category="positive" count={!isFinite(positivePercentage) ? "-" : positivePercentage + "%"} />
     </div>
-  );
+  )
 }
 
-export default App;
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+export default App
